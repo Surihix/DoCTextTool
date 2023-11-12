491a268abf621668e523630ac9817052e995ade7
Surihix <neweramysterymailer@gmail.com>
2023-11-12 09:33:09 +0530

    added utf-8 conversion

--- DoCTextTool/LineClasses/LinesParser.cs
+++ DoCTextTool/LineClasses/LinesParser.cs
@@ -3,6 +3,7 @@ using Ionic.Zlib;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 
 namespace DoCTextTool.LineClasses
 {
@@ -10,12 +11,14 @@ namespace DoCTextTool.LineClasses
     {
         public static void ExtractLines(Stream decryptedStream, bool isCompressed, uint bodySize, ushort lineCount, string outFile)
         {
-            using (var outStream = new MemoryStream())
+            outFile.IfFileExistsDel();
+
+            using (var outBinStream = new MemoryStream())
             {
-                using (var outStreamReader = new BinaryReader(outStream))
+                using (var outBinReader = new BinaryReader(outBinStream))
                 {
                     decryptedStream.Seek(0, SeekOrigin.Begin);
-                    decryptedStream.CopyInBuffers(outStream, 32);
+                    decryptedStream.CopyInBuffers(outBinStream, 32);
 
                     decryptedStream.Seek(32, SeekOrigin.Begin);
 
@@ -24,18 +27,18 @@ namespace DoCTextTool.LineClasses
                         case true:
                             using (ZlibStream decompressor = new ZlibStream(decryptedStream, CompressionMode.Decompress, true))
                             {
-                                decompressor.CopyTo(outStream);
+                                decompressor.CopyTo(outBinStream);
                             }
                             break;
 
                         case false:
-                            decryptedStream.CopyInBuffers(outStream, bodySize);
+                            decryptedStream.CopyInBuffers(outBinStream, bodySize);
                             break;
                     }
 
-                    using (var outFileStream = new FileStream(outFile, FileMode.Append, FileAccess.Write))
+                    using (var outTxtBinStream = new FileStream(outFile, FileMode.Append, FileAccess.Write))
                     {
-                        using (var outFileWriter = new BinaryWriter(outFileStream))
+                        using (var outTxtBinWriter = new BinaryWriter(outTxtBinStream))
                         {
 
                             var lineOffsets = new FileStructs.LineOffsets();
@@ -45,14 +48,14 @@ namespace DoCTextTool.LineClasses
                             for (int l = 0; l < lineCount; l++)
                             {
                                 // Get offsets
-                                outStreamReader.BaseStream.Position = readPos;
-                                lineOffsets.UnknownId = outStreamReader.ReadUInt32();
+                                outBinReader.BaseStream.Position = readPos;
+                                lineOffsets.UnknownId = outBinReader.ReadUInt32();
 
-                                outStreamReader.BaseStream.Position = readPos + 4;
-                                lineOffsets.LineIdOffset = outStreamReader.ReadUInt32();
+                                outBinReader.BaseStream.Position = readPos + 4;
+                                lineOffsets.LineIdOffset = outBinReader.ReadUInt32();
 
-                                outStreamReader.BaseStream.Position = readPos + 8;
-                                lineOffsets.LineOffset = outStreamReader.ReadUInt32();
+                                outBinReader.BaseStream.Position = readPos + 8;
+                                lineOffsets.LineOffset = outBinReader.ReadUInt32();
 
                                 // Write UnkID
                                 var unkId = Convert.ToString(lineOffsets.UnknownId);
@@ -61,33 +64,40 @@ namespace DoCTextTool.LineClasses
                                 {
                                     unkIdList.Add(Convert.ToByte(num));
                                 }
-                                WriteEachByte(unkIdList, outFileWriter, ref writePos);
-                                WriteSeparator(outFileWriter, writePos);
-                                writePos = (uint)outFileWriter.BaseStream.Position;
+                                WriteEachByte(unkIdList, outTxtBinWriter, ref writePos);
+                                WriteSeparator(outTxtBinWriter, writePos);
+                                writePos = (uint)outTxtBinWriter.BaseStream.Position;
 
                                 // Write Line ID
-                                outStreamReader.BaseStream.Position = lineOffsets.LineIdOffset;
-                                var lineIdBytesList = outStreamReader.ReadBytesTillNull();
-                                WriteEachByte(lineIdBytesList, outFileWriter, ref writePos);
-                                WriteSeparator(outFileWriter, writePos);
-                                writePos = (uint)outFileWriter.BaseStream.Position;
+                                outBinReader.BaseStream.Position = lineOffsets.LineIdOffset;
+                                var lineIdBytesList = outBinReader.ReadBytesTillNull();
+                                WriteEachByte(lineIdBytesList, outTxtBinWriter, ref writePos);
+                                WriteSeparator(outTxtBinWriter, writePos);
+                                writePos = (uint)outTxtBinWriter.BaseStream.Position;
 
                                 // Write Line
-                                outStreamReader.BaseStream.Position = lineOffsets.LineOffset;
-                                var lineBytesList = outStreamReader.ReadBytesTillNull();
-                                WriteEachByte(lineBytesList, outFileWriter, ref writePos);
-                                writePos = (uint)outFileWriter.BaseStream.Position;
+                                outBinReader.BaseStream.Position = lineOffsets.LineOffset;
+                                var lineBytesList = outBinReader.ReadBytesTillNull();
+                                WriteEachByte(lineBytesList, outTxtBinWriter, ref writePos);
+                                writePos = (uint)outTxtBinWriter.BaseStream.Position;
 
                                 // Move to next line
-                                outFileWriter.BaseStream.Position = writePos;
-                                outFileWriter.Write((byte)13);
-                                outFileWriter.Write((byte)10);
-                                writePos = (uint)outFileWriter.BaseStream.Position;
+                                outTxtBinWriter.BaseStream.Position = writePos;
+                                outTxtBinWriter.Write((byte)13);
+                                outTxtBinWriter.Write((byte)10);
+                                writePos = (uint)outTxtBinWriter.BaseStream.Position;
 
                                 readPos += 12;
                             }
                         }
                     }
+
+                    // Convert the txt file to UTF-8
+                    var outTxtBinArray = File.ReadAllBytes(outFile);
+                    var outTxtBinConverted = Encoding.Convert(Encoding.GetEncoding("shift-jis"), Encoding.UTF8, outTxtBinArray);
+
+                    File.Delete(outFile);
+                    File.WriteAllBytes(outFile, outTxtBinConverted);
                 }
             }
         }
