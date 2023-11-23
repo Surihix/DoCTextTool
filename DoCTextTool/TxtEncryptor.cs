14dc88076e98e2e24a9bb99e543312ceb47af678
Surihix <neweramysterymailer@gmail.com>
2023-11-23 13:18:15 +0530

    add encryption function

--- DoCTextTool/TxtEncryptor.cs
+++ DoCTextTool/TxtEncryptor.cs
@@ -13,9 +13,12 @@ namespace DoCTextTool
 
             var outFile = Path.Combine(Path.GetDirectoryName(inFile), $"{Path.GetFileNameWithoutExtension(inFile)}.enc");
 
-            using (var )
+            using (var inFileStream = new FileStream(inFile, FileMode.Open, FileAccess.Read))
             {
+                using (var inFileReader = new BinaryReader(inFileStream))
+                {
 
+                }
             }
 
 
